7882fdcb7da6570cceebf9fec37d44b9c4894a2b
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2016-07-28 15:40:18 +0200

    fix error in clean_linux.sh

--- input_files/clean_linux.sh
+++ input_files/clean_linux.sh
@@ -4,7 +4,7 @@
 # clean -Benchmarks
 # by WW: 10.03.2006
 # --------------------------------------------------------------
-find . \( -name '*.tec'\) -exec rm {} \;
+find . \( -name '*.tec' \) -exec rm {} \;
 find . \( -name '*.vtk' -or -name '*.vtu' -or -name '*.pvd' \) -exec rm {} \;
 find . \( -name '*.pet' -or -name '*.log' \) -exec rm -rf {} \;
 
