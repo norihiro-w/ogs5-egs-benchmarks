c3895b42e77dfd6a22041cb1e010d6e41fccced5
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2016-08-16 07:20:58 +0200

    rm also csv in clean_linux.sh

--- input_files/clean_linux.sh
+++ input_files/clean_linux.sh
@@ -5,6 +5,7 @@
 # by WW: 10.03.2006
 # --------------------------------------------------------------
 find . \( -name '*.tec' \) -exec rm {} \;
+find . \( -name '*.csv' \) -exec rm {} \;
 find . \( -name '*.vtk' -or -name '*.vtu' -or -name '*.pvd' \) -exec rm {} \;
 find . \( -name '*.pet' -or -name '*.log' \) -exec rm -rf {} \;
 
